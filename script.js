52016eed2123fdc6d48fd76a5fe1905c27f67720
ynsuldg <yns.uldg@hotmail.com>
2026-06-30 23:28:32 +0200

    Initial portfolio website.

--- script.js
+++ script.js
@@ -2,11 +2,11 @@ const menuToggle = document.querySelector(".menu-toggle");
 const navLinks = document.querySelector(".nav-links");
 
 menuToggle.addEventListener("click", () => {
-  navLinks.classList.toggle("open");
+    navLinks.classList.toggle("open");
 });
 
 document.querySelectorAll(".nav-links a").forEach((link) => {
-  link.addEventListener("click", () => {
-    navLinks.classList.remove("open");
-  });
+    link.addEventListener("click", () => {
+        navLinks.classList.remove("open");
+    });
 });
